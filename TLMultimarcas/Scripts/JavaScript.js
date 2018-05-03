bed97ae7bb8af2f726ad77ef60b9a5177f4e8bb6
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-05-03 13:24:08 -0300

    #16492 - Edição das tables dos veiculos e script para busca

--- TLMultimarcas/Scripts/JavaScript.js
+++ TLMultimarcas/Scripts/JavaScript.js
@@ -5,7 +5,17 @@
 $('#selectMarca').change(function () {
     var e = document.getElementById("selectMarca");
     var itemSelecionado = e.options[e.selectedIndex].value;
-    if (itemSelecionado != '') {
+    var select = document.getElementById('selectModelo');
+    var optionSelect = document.createElement('option');
+    select.options.length = 0;
+    optionSelect.innerHTML = "Selecione";
+    select.appendChild(optionSelect);
+    if (itemSelecionado == 'Chevrolet') {
         console.log(itemSelecionado);
+        //for
+        var optionCar = document.createElement('option');
+        optionCar.value = "S10";
+        optionCar.innerHTML = "S10";
+        select.appendChild(optionCar);
     }
 });
