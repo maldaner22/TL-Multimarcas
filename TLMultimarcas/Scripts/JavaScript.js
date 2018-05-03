f42bcf39d9072b4e9bced0f1351fe0721c4774d2
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-05-03 13:37:50 -0300

    #16492 - Switch case para setar valores nos selects

--- TLMultimarcas/Scripts/JavaScript.js
+++ TLMultimarcas/Scripts/JavaScript.js
@@ -1,21 +1,38 @@
-﻿$("#btnFooter").click(function () {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-});
-
-$('#selectMarca').change(function () {
-    var e = document.getElementById("selectMarca");
-    var itemSelecionado = e.options[e.selectedIndex].value;
+﻿function showModels(brand) {
     var select = document.getElementById('selectModelo');
     var optionSelect = document.createElement('option');
     select.options.length = 0;
     optionSelect.innerHTML = "Selecione";
     select.appendChild(optionSelect);
-    if (itemSelecionado == 'Chevrolet') {
-        console.log(itemSelecionado);
-        //for
-        var optionCar = document.createElement('option');
-        optionCar.value = "S10";
-        optionCar.innerHTML = "S10";
-        select.appendChild(optionCar);
+    console.log(brand);
+    switch (brand) {
+        case 'Chevrolet':
+            var optionCar = document.createElement('option');
+            optionCar.value = "S10";
+            optionCar.innerHTML = "S10";
+            select.appendChild(optionCar);
+            break;
+        case 'Citroën':
+            var optionCar = document.createElement('option');
+            optionCar.value = "C3";
+            optionCar.innerHTML = "C3";
+            select.appendChild(optionCar);
+            break;
     }
+}
+
+function actions() {
+    $("#btnFooter").click(function () {
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    });
+
+    $('#selectMarca').change(function () {
+        var e = document.getElementById("selectMarca");
+        var itemSelecionado = e.options[e.selectedIndex].value;
+        showModels(itemSelecionado);
+    });
+}
+
+$(document).ready(function () {
+    actions();
 });
