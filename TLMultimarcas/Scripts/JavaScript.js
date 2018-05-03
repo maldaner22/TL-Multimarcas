6fd81029bc31dda5fc578d0f6f9f7ebd1c6568ae
Luis Kroetz <luis.kroetz@digitaldesk.com.br>
2018-05-03 11:27:24 -0300

    #16492 - Adicionado arquivo JavaScript

---
+++ TLMultimarcas/Scripts/JavaScript.js
@@ -0,0 +1,11 @@
+﻿$("#btnFooter").click(function () {
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+});
+
+$('#selectMarca').change(function () {
+    var e = document.getElementById("selectMarca");
+    var itemSelecionado = e.options[e.selectedIndex].value;
+    if (itemSelecionado != '') {
+        console.log(itemSelecionado);
+    }
+});
