7d8322ed90c960017b57a7e2a380c55030662e7e
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-05-08 15:16:53 -0300

    #16492 - Grade de veículos

--- TLMultimarcas/Controllers/AdministrativoController.cs
+++ TLMultimarcas/Controllers/AdministrativoController.cs
@@ -105,16 +105,16 @@ namespace TLMultimarcas.Controllers
 
         public ActionResult AdicionarFuncionario()
         {
-            ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario");
-            ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario");
-            ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario");
-            ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario");
-            ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
-            ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
-            ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
-            ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
-            ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
-            ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
+            //ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario");
+            //ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario");
+            //ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario");
+            //ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario");
+            //ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
+            //ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
+            //ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
+            //ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
+            //ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
+            //ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
             return View();
         }
 
@@ -127,16 +127,16 @@ namespace TLMultimarcas.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario");
-            ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario");
-            ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario");
-            ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario");
-            ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
-            ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
-            ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
-            ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
-            ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
-            ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
+            //ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario", usuario.NomeFuncionario);
+            //ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario", usuario.LoginFuncionario);
+            //ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario", usuario.SenhaFuncionario);
+            //ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario", usuario.EmailFuncionario);
+            //ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
+            //ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
+            //ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
+            //ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
+            //ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
+            //ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
             return View(usuario);
         }
     }
