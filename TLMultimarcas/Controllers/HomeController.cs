d63ebbe28c2f1834404bd022b27d747c9064921a
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-05-09 11:19:53 -0300

    #16492 - Select das marcas obtem os valores por uma lista

--- TLMultimarcas/Controllers/HomeController.cs
+++ TLMultimarcas/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,8 +14,12 @@ namespace TLMultimarcas.Controllers
 
         public ActionResult Index()
         {
-            var brands = db.Marca;
-            return View(brands);
+            //var brands = db.Marca;
+            //return View(brands);
+            var brands = db.Marca.ToList();
+            SelectList list = new SelectList(brands, "IdMarca", "NomeMarca");
+            ViewBag.brandsname = list;
+            return View();
         }
     }
 }
