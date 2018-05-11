a7c6ba112dbcf7c69657299c377b06727112557a
Luis Kroetz <luis.kroetz@digitaldesk.com.br>
2018-05-11 14:46:03 -0300

    #16492 - Adicionar Funcionário

--- TLMultimarcas/Controllers/AdministrativoController.cs
+++ TLMultimarcas/Controllers/AdministrativoController.cs
@@ -96,48 +96,5 @@ namespace TLMultimarcas.Controllers
                 return Boolean.FalseString;
             }
         }
-
-        public ActionResult Funcionarios()
-        {
-            var users = db.Usuario;
-            return View(users);
-        }
-
-        public ActionResult AdicionarFuncionario()
-        {
-            ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario");
-            ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario");
-            ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario");
-            ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario");
-            ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
-            ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
-            ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
-            ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
-            ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
-            ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AdicionarFuncionarios(Usuario usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Usuario.Add(usuario);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.NomeFuncionario = new SelectList(db.Usuario, "NomeFuncionario", usuario.NomeFuncionario);
-            ViewBag.LoginFuncionario = new SelectList(db.Usuario, "LoginFuncionario", usuario.LoginFuncionario);
-            ViewBag.SenhaFuncionario = new SelectList(db.Usuario, "SenhaFuncionario", usuario.SenhaFuncionario);
-            ViewBag.EmailFuncionario = new SelectList(db.Usuario, "EmailFuncionario", usuario.EmailFuncionario);
-            ViewBag.AdicionarFuncionario = new SelectList(db.Usuario, "AdicionarFuncionario");
-            ViewBag.AdicionarVeiculo = new SelectList(db.Usuario, "AdicionarVeiculo");
-            ViewBag.EditarFuncionario = new SelectList(db.Usuario, "EditarFuncionario");
-            ViewBag.EditarVeiculo = new SelectList(db.Usuario, "EditarVeiculo");
-            ViewBag.ExcluirFuncionario = new SelectList(db.Usuario, "ExcluirFuncionario");
-            ViewBag.ExcluirVeiculo = new SelectList(db.Usuario, "ExcluirVeiculo");
-            return View(usuario);
-        }
     }
 }
---
+++ TLMultimarcas/Controllers/FuncionariosController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TLMultimarcas.Models;
+
+namespace TLMultimarcas.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private TLMultimarcasEntities db = new TLMultimarcasEntities();
+
+        public ActionResult Index()
+        {
+            var users = db.Usuario;
+            return View(users);
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+    }
+}
